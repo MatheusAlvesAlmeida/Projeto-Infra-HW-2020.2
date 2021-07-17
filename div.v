81b10fa7f406f118c7bcef9565e544aecd35fde9
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2021-07-17 09:54:06 -0300

    feat: inicio da implementacao do div

---
+++ div.v
@@ -0,0 +1,14 @@
+module divisor(
+    input wire clock,
+    input wire reset,
+    input wire multOrDiv,
+    input signed [31:0] A,
+    input signed [31:0] B,
+    output reg [31:0] hi, 
+    output reg [31:0] lo,
+    output reg div0, 
+);
+
+
+
+endmodule;
