84af8cb7799190863de2f48a9108892f004ef58a
Gustavo F. de Farias <gff@cin.ufpe.br>
2021-07-30 14:00:36 -0300

    Sizes prontos

--- parts_made/sizes/det_size.v
+++ parts_made/sizes/det_size.v
@@ -4,6 +4,8 @@ module det_size (
   output wire [31:0] data_out,
 );
 
-  assign data_out = 
+  wire [31:0] aux;
+  assign aux = DetSizeCtrl[0] ? {{24{1'b0}}, data_in[7:0]} : data_in;
+  assign data_out = DetSizeCtrl[1] ? {{16{1'b0}}, data_in[15:0]} : aux;
 
 endmodule
--- parts_made/sizes/set_size.v
+++ parts_made/sizes/set_size.v
@@ -1,8 +1,13 @@
 module set_size (
-  input  wire [1:0] SetSizeCtrl,
-  output wire [27:0] data_out,
+  input  wire [1:0]  SetSizeCtrl,
+  input  wire [31:0] data_in_0,  // MDR_out
+  input  wire [31:0] data_in_1,  // B_out
+  output wire [31:0] data_out,
 );
+  wire [31:0] aux;
 
-  assign data_out = 
+  assign aux      = SetSizeCtrl[0] ? {data_in_0[31:8], data_in_1[7:0]}   : data_in_1;
+
+  assign data_out = SetSizeCtrl[1] ? {data_in_0[31:16], data_in_1[15:0]} : aux;
 
 endmodule
