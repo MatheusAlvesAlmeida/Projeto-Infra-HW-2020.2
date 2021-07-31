aae872fa934d61fe7104f0dcb6db4fac0475af6d
Clara <mcdag@cin.ufpe,br>
2021-07-30 23:38:43 -0300

    Adiçao do concatenate --- rever como as exceçoes estao sendo feitas

--- parts_made/concatenar/concatenate.v
+++ parts_made/concatenar/concatenate.v
@@ -1,5 +1,14 @@
 module concatenate(
-  
+  input wire        DoConcatenate;
+  input wire [27:0] data_in_1;
+  input wire [31:0] data_in_2;
+
+  input wire [31:0] data_out;
 );
+
+  wire [3:0] aux;
+  assign aux = [3:0] data_in_2;
+
+  assign data_out = {data_in_1, aux};
 
 endmodule
