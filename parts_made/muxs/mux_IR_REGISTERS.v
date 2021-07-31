133b8f041f49df57f27f244fc5a80fea03845066
pietromasur <pietromasur@hotmail.com>
2021-07-31 14:48:37 -0300

    Resolvendo erro no muxIR_register e mudando RegDst para 2'b em todas as instruçoes da ctrl_unit

--- parts_made/muxs/mux_IR_REGISTERS.v
+++ parts_made/muxs/mux_IR_REGISTERS.v
@@ -10,7 +10,7 @@ module mux_IR_REGISTERS (
   wire [31:0] aux_2;
 
   assign aux_1 = RegDist[0] ? input_2 : input_1;
-  assign aux_2 = RegDist[0] ? 32'b00000000000000000000000000011111 : 32'b00000000000000000000000000100000;
+  assign aux_2 = RegDist[0] ? 32'b00000000000000000000000000011110 : 32'b0000000000000000000000000011111;
 
   assign result = RegDst[1] ? aux_2 : aux_1;
 
